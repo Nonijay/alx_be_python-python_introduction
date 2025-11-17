4585965b5038f5373b63930cbc1a792b96172ec8
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-11-17 12:04:16 +0100

    updated with error handling

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,11 +5,18 @@ type_of_operation = input("Choose the operation (+, -, *, /): ")
 
 match type_of_operation:
     case "+":
-        print(f"the result is {num1 + num2}")
+        print(f"the result is: {num1 + num2}")
     case "-":
-        print (f"the result is {num1 - num2}")
+        print (f"the result is: {num1 - num2}")
     case "*":
-        print (f"the result is {num1 * num2}")
+        print (f"the result is: {num1 * num2}")
     case "/":
-        print (f"the result is {num1 / num2}")
+        if num2 != 0:
+            print (f"the result is: {num1 / num2}")
+        else : 
+            # Error Handling for division of 0
+            print ("Error: cannot divide by zero")
+    case _:
+        print ("Error: Enter a valid operation choice. please use +, -, *, /.")
+    
     
