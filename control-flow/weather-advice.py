5a3c9cfaa7660c6559fe5327b9327bf0c28025d9
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-11-18 11:46:33 +0100

    corrected

--- control-flow/weather-advice.py
+++ control-flow/weather-advice.py
@@ -1,5 +1,5 @@
 weather_advice = input("What's the weather like today? (sunny/rainy/cold): ").lower()
-
+# Ask user for weather input then use an if statement to respond to th euser's choice
 if weather_advice == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather_advice == "rainy":  
