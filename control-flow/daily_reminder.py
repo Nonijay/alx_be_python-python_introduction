88c7b3d6fb218c90c1aeae18edab4f7c484beded
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-11-18 15:06:38 +0100

    updated variable name for autochecker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,23 @@
-daily_task = input("Enter your task: ")
+task = input("Enter your task: ")
+time_bound = input("Is it time-bound? (yes or no): ")
 priority = input("what is your task priority (high, medium, low): ")
-time_bound = input("Is it time-bound (yes or no): ")
+
 
 match priority :
     case  "high":
         if time_bound == "yes":
-            print(f"{daily_task} is a time-senstive high Priority task that requires immediate attention today")
+            print(f"{task} is a time-senstive high Priority task that requires immediate attention today")
         else:
-            print(f"{daily_task} is a high priority but can be dealt in your convinience")
+            print(f"{task} is a high priority but can be dealt in your convinience")
     case "medium":
         if time_bound == "yes":
-            print(f"{daily_task} is a time-senstive medium Priority task that requires immediate attention today")
+            print(f"{task} is a time-senstive medium Priority task that requires immediate attention today")
         else: 
-            print(f"{daily_task} is a medium priority but can be dealt in your convinience")
+            print(f"{task} is a medium priority but can be dealt in your convinience")
     case "low":
         if time_bound == "yes":
-            print(f"{daily_task} is a time-senstive low Priority task that requires immediate attention today")
+            print(f"{task} is a time-senstive low Priority task that requires immediate attention today")
         else: 
-            print(f"{daily_task} is a low priority but can be dealt in your convenience")
+            print(f"{task} is a low priority but can be dealt in your convenience")
     case _:
         print(f"please enter a valid task-priority")
