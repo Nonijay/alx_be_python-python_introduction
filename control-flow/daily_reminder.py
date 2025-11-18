47e76af526aaad57692e0aae29d234bf5b49b693
Chukwu Chinonso Jeffrey <89136575+Nonijay@users.noreply.github.com>
2025-11-18 16:30:34 +0100

    updated feedback message

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,18 +6,18 @@ priority = input("Priority (high/medium/low): ").lower()
 match priority :
     case  "high":
         if time_bound == "yes":
-            print(f"{task} is a time-senstive high Priority task that requires immediate attention today")
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
-            print(f"{task} is a high priority but can be dealt in your convinience")
+            print(f"Note: '{task}' is a high priority task. Consider completing it when you have free time.")
     case "medium":
         if time_bound == "yes":
-            print(f"{task} is a time-senstive medium Priority task that requires immediate attention today")
+            print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
         else: 
-            print(f"{task} is a medium priority but can be dealt in your convinience")
+            print(f"Note: '{task}' is a medium priority task. Consider completing it when you have free time.")
     case "low":
         if time_bound == "yes":
-            print(f"{task} is a time-senstive low Priority task that requires immediate attention today")
+            print(f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
         else: 
-            print(f"{task} is a low priority but can be dealt in your convenience")
+            print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
     case _:
-        print(f"please enter a valid task-priority")
+        print(f"please enter a valid priority requirement")
